921e7ee2d0e3d531fc72db85179d82f6f793e164
anirban00537 <anirban00537@gmail.com>
2021-12-18 18:30:51 +0600

    card changes

--- mobile/components/home/card.js
+++ mobile/components/home/card.js
@@ -4,11 +4,9 @@ import {Button, Card, Layout, Text} from '@ui-kitten/components';
 
 const card = () => {
   return (
-    <Layout style={styles.layout}>
-      <Card style={styles.card}>
-        <Text>With Header</Text>
-      </Card>
-    </Layout>
+    <Card style={styles.card}>
+      <Text>With Header</Text>
+    </Card>
   );
 };
 
@@ -25,13 +23,6 @@ const styles = StyleSheet.create({
   },
   card: {
     flex: 1,
-    margin: 2,
-  },
-  footerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
-  footerControl: {
-    marginHorizontal: 2,
+    margin: 8,
   },
 });
