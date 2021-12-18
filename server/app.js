fcefb8f043acfd6ab53fe95aa3948a5c1e63da9e
anirban00537 <anirban00537@gmail.com>
2021-12-18 15:02:38 +0600

    setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-// express js boiler plate
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log("Server is running on port 5000");
+});
