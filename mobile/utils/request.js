157195ec27fd3be7c3662c7b8fd62dc34320dcd9
anirban00537 <anirban00537@gmail.com>
2021-12-21 21:00:34 +0600

    axios base setup

---
+++ mobile/utils/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://192.168.0.108:3000';
+
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = '';
+  return config;
+});
+
+axios.interceptors.response.use(response => {
+  return response;
+});
+
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response.status === 401) {
+      //   localStorage.removeItem('token');
+      //   window.location.reload();
+    }
+    return Promise.reject(error);
+  },
+);
