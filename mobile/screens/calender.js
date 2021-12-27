72026f8908c1382bd2a16ec5c6b9094f541d1b6d
anirban00537 <anirban00537@gmail.com>
2021-12-27 23:09:23 +0600

    date added

--- mobile/screens/calender.js
+++ mobile/screens/calender.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import {Layout, Text} from '@ui-kitten/components';
-
+import {Layout, Text, Calendar} from '@ui-kitten/components';
+import {StyleSheet} from 'react-native';
 const calender = () => {
+  const [date, setDate] = React.useState(new Date());
+
   return (
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text category="h1">calender</Text>
+    <Layout style={styles.container}>
+      <Calendar
+        date={date}
+        onSelect={nextDate => setDate(nextDate)}
+        style={styles.calender}
+        dateStyle={styles.date}
+        textStyle={styles.text}
+        selectedDateStyle={styles.selectedDate}
+        selectedTextStyle={styles.selectedText}
+        disabledDateStyle={styles.disabledDate}
+        disabledTextStyle={styles.disabledText}
+      />
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  calender: {
+    width: '100%',
+    height: '100%',
+  },
+  date: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  selectedDate: {
+    backgroundColor: '#008080',
+  },
+  selectedText: {
+    color: '#fff',
+  },
+  disabledDate: {
+    backgroundColor: '#fff',
+  },
+  disabledText: {
+    color: '#000',
+  },
+});
 
 export default calender;
